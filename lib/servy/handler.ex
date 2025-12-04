8bcf03f235187daf1dc7f4e9bb7601a2e92c05d1
Gian Carlo I. Gilos <gian@isr.com.ph>
2025-12-04 12:27:10 +0800

    wip: finished ch 2 & 3 of tutorial

---
+++ lib/servy/handler.ex
@@ -0,0 +1,45 @@
+defmodule Servy.Handler do
+  def handle_request(request) do
+    request
+    |> parse()
+    |> route()
+    |> format_response()
+  end
+
+  def parse(request) do
+    conv = %{method: "GET", path: "/wildthings", resp_body: ""}
+  end
+
+  def route(conv) do
+    conv = %{method: "GET", path: "/wildthings", resp_body: "Owlbears, Beholders, Dragons"}
+  end
+
+  def format_response(conv) do
+    """
+    HTTP/1.1 200 OK
+    Content-Type: text/html
+    Content-Length: 20
+
+    Owlbears, Beholders, Dragons
+    """
+  end
+end
+
+request = """
+GET /wildthings HTTP/1.1
+Host: example.com
+User-Agent: ExampleBrowser/1.0
+Accept: */*
+
+"""
+
+expected_response = """
+HTTP/1.1 200 OK
+Content-Type: text/html
+Content-Length: 20
+
+Owlbears, Beholders, Dragons
+"""
+
+response = Servy.Handler.handle_request(request)
+IO.puts(response)
